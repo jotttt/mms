eabccd1902ae3424824ac67ca8b037ee3db5f51f
jotttt <jlaulik@gmail.com>
2020-03-21 17:08:53 +0200

    chage default sort products by date added

--- dist/catalog/controller/product/category.php
+++ dist/catalog/controller/product/category.php
@@ -18,7 +18,7 @@ class ControllerProductCategory extends Controller {
 		if (isset($this->request->get['sort'])) {
 			$sort = $this->request->get['sort'];
 		} else {
-			$sort = 'p.sort_order';
+			$sort = 'p.product_id';
 		}
 
 		if (isset($this->request->get['order'])) {
